da92ca48253ae232b6c5187522fbda3d18d10144
netxs139 <netxs@139.com>
2026-09-12 09:23:30 +0800

    Add configuration constants for OCI retry

---
+++ oci_retry.py
@@ -0,0 +1,7 @@
+COMPARTMENT_ID = "ocid1.tenancy.oc1..your-ocid"
+SSH_PUBLIC_KEY = "ssh-rsa AAAA...your-public-key"
+INSTANCE_NAME = "streamlit-server"
+ARM_OCPUS = 2
+ARM_MEMORY_IN_GBS = 12
+BOOT_VOLUME_SIZE_IN_GBS = 100
+RETRY_INTERVAL = 90  # Retry interval in seconds
